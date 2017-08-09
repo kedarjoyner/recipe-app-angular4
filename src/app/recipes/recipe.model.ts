721ac52eff56c88815f9b7ce62bb056b00911ee4
kedarjoyner <kedarjoyner@gmail.com>
2017-08-09 17:02:54 -0500

    create recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+// a blueprint for objects we create
+export class Recipe {
+    // public because want to access outside of instatiating this
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}
