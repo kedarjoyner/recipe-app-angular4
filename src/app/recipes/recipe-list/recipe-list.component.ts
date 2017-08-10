c81ee243d829415b42db8386184a212f9090ac25
kedarjoyner <kedarjoyner@gmail.com>
2017-08-09 19:25:05 -0500

    display recipe list array, outline recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  recipes = [];
+  // colon indicates to typescript that we will be indicated the type
+  // use array of Recipe object defined in model
+  recipes: Recipe[] = [
+    //create new Recipe object, which is calling from the constructor in recipe.model.ts
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://s-media-cache-ak0.pinimg.com/736x/c0/4e/2f/c04e2f4a2d474a0d02188891cdacc83b--new-york-street-food-new-york-city-food.jpg')
+  ];
 
   constructor() { }
 
